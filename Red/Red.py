d442df433655105fca9804f2fdfd1db2ecfbf5c1
Ad4m Wayn3 <misterwayne04@gmail.com>
2024-09-20 21:02:10 -0500

    adicion heuristica y matriz de probabilidades

--- Red/Red.py
+++ Red/Red.py
@@ -3,29 +3,28 @@ import random
 import sys
 import time
 
+ROUTER_COUNT = 20 # se asume por ahora un total de 20 routers
+
+# genera matriz de adyacencias con probabilidades de caída de enlace
+def genMatrix(n: int):
+    rd = random
+    prob = lambda p: p if (p <= 0.2) else 1
+    return [ [prob(rd.uniform(0,1)) for i in range(0,n)] 
+        for j in range(0,n)]
+
+def adjacent(xs):
+    out = []
+    for i in range(0,len(xs)): 
+        if xs[i]<1: out.append(i)
+    return out
+
+matriz = genMatrix(ROUTER_COUNT)
+nombresRouters = ["Router" + str(i+1) for i in range(0,ROUTER_COUNT)]
 diccionario_conexiones = {
-    "Router1": ["Router3"],
-    "Router2": ["Router3", "Router9"],
-    "Router3": ["Router1", "Router2", "Router4", "Router5"],
-    "Router4": ["Router3", "Router5", "Router6"],
-    "Router5": ["Router3", "Router4", "Router6", "Router9"],
-    "Router6": ["Router4", "Router5", "Router7", "Router13"],
-    "Router7": ["Router6", "Router8", "Router13"],
-    "Router8": ["Router7", "Router9", "Router11"],
-    "Router9": ["Router2", "Router5", "Router8", "Router10"],
-    "Router10": ["Router9", "Router11", "Router16"],
-    "Router11": ["Router8", "Router10", "Router12", "Router16"],
-    "Router12": ["Router11", "Router13", "Router14"],
-    "Router13": ["Router6", "Router7", "Router12", "Router15"],
-    "Router14": ["Router12", "Router15", "Router16"],
-    "Router15": ["Router13", "Router14", "Router18"],
-    "Router16": ["Router10", "Router11", "Router14", "Router18", "Router19"],
-    "Router17": ["Router18", "Router19"],
-    "Router18": ["Router15", "Router16", "Router17", "Router20"],
-    "Router19": ["Router16", "Router17"],
-    "Router20": ["Router18"],
+    nombresRouters[i] : ["Router"+str(j+1) for j in adjacent(matriz[i])]
+    for i in range(0,ROUTER_COUNT)
 }
-matriz = [[0] * 20 for i in range(20)]
+
 conexiones_revisadas = []
 
 
@@ -54,6 +53,12 @@ def crear_matriz():
     # print("Matriz de adyacencia creada:\n ")
     return matriz
 
+
+def heuristic(matrix, bandwiths, n, m):
+    prob = lambda p: 1 if (rd.uniform(0,1) >= p) else 0
+    denominator = prob(matrix[n][m]) * (1 - matrix[n][m])
+    if (denominator == 0): return BIG_NUM
+    else: return (bandwiths[n] + bandwiths[m]) / denominator
 
 def heuristica(RouterInicial, RouterFinal):
     h = 0
@@ -154,6 +159,8 @@ def Dijkstra(origen, destino, matriz_adyacencia, routers):
     # Devuelve la distancia más corta desde el nodo origen al nodo destino y el camino
     return camino
 
+for nombre in nombresRouters:
+    print(diccionario_conexiones[nombre])
 
 Routers = [None] * 20
 
--- Red/Router.py
+++ Red/Router.py
@@ -1,4 +1,4 @@
-import random
+import random as rd
 
 class Router:
     def __init__(self, name, ancho_banda, conexiones):
@@ -7,8 +7,10 @@ class Router:
         self.conexiones = conexiones
     
     def caido(self):
-        caido = random.random()
+        caido = rd.random()
         # print(caido)
         if  caido <0.2:
             return 0
         return 1
+
+BIG_NUM = 1000000
