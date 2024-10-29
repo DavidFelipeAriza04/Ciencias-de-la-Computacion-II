b1325153d48bacc44663c7cd3e29402e09aad37e
Ad4mWayn3 <109969731+Ad4mWayn3@users.noreply.github.com>
2024-10-29 11:51:34 -0500

    Add files via upload

--- graph_coloring.py
+++ graph_coloring.py
@@ -1,8 +1,23 @@
+# Asignatura de Ciencias de la Computación II
+# 	Integrantes:						Código:
+#	David Felipe Ariza Ariza			20221020029
+#	Javier Alejandro Penagos Hernandez	20221020028
+#	Oliver Duarte Ramirez				20221020018
+
 class Graph:
 	def __init__(self, vertices: list[int]):
-		self.vertices = vertices
+		self.vertices = vertices # lista de vertices por etiqueta
 		self.adjacencies = [[0] * len(vertices) for _ in vertices]
 	
+	# modificar y transponer la lista de adyacencias (asumiendo grafo
+	#	no dirijido.
+	def set_adj(self, adj):
+		self.adjacencies = adj
+		for i in range(len(self.vertices)):
+			for j in range(len(self.vertices)):
+				self.adjacencies[j][i] = self.adjacencies[i][j]
+	
+	# especificar una arista mediante dos vértices y un peso
 	def add_edge(self, u: int, v: int, weight=1):
 		vert = self.vertices
 		for i in range(len(vert)):
@@ -12,6 +27,7 @@ class Graph:
 		self.adjacencies[v][u] = weight
 		return self
 	
+	# añade un vértice a la lista y expande la matríz de adyacencias.
 	def add_vertex(self, vertex: int):
 		if vertex in self.vertices: return self
 		for row in self.adjacencies:
@@ -20,10 +36,13 @@ class Graph:
 		self.adjacencies.append([0] * len(self.vertices))
 		return self
 	
+	# obtiene el índice de un vértice en la lista
 	def get_v_index(self, vertex: int) -> int:
 		for i in range(len(self.vertices)):
 			if self.vertices[i] == vertex: return i
 	
+	# obtiene una lista de adyacencias donde cada arista implica la
+	#	necesidad de colores distintos entre los dos nodos
 	def get_incompatibility_graph(self):
 		adj_list = [[] for _ in self.vertices]
 		for i in range(len(self.vertices)):
@@ -32,18 +51,58 @@ class Graph:
 					adj_list[i].append(self.vertices[j])
 		return adj_list
 	
+	# retorna una lista de numeros donde cada posicion representa un color
+	#	para cada vértice.
 	def greedy_coloring(self) -> list[int]:
-		pass
+		v_count = len(self.vertices)
+		graph = self.get_incompatibility_graph()
+
+		result = [-1] * v_count
+		result[0] = 0
+		available = [False] * v_count
+
+		for u in range(1, v_count):
+			for v in graph[u]:
+				v_idx = self.get_v_index(v)
+				if result[v_idx] != -1:
+					available[result[v_idx]] = True
+			
+			for color in range(v_count):
+				if not available[color]:
+					break
+			
+			result[u] = color
+
+			for v in graph[u]:
+				v_idx = self.get_v_index(v)
+				if result[v_idx] != -1:
+					available[result[v_idx]] = False
+
+		return result
 
 def main():
-	graph = Graph([3,4,5,6])
-	graph.add_edge(4,5).add_edge(3,4,40).add_vertex(5).add_edge(3,6,144)
+	graph = Graph([1,2,3,4,5,6])
+	graph.set_adj([
+		[0,85,175,200,50,100],
+		[0,0,125,175,100,160],
+		[0,0,0,100,200,250],
+		[0,0,0,0,210,220],
+		[0,0,0,0,0,100],
+		[0,0,0,0,0,0]
+	])
 	g = graph.get_incompatibility_graph()
+
+	# se imprime la matríz de adyacencias
 	for row in graph.adjacencies:
 		print(row)
+
 	print("")
+
+	# se imprime la lista de adyacencias del grafo de incompatibilidad
 	for row in g:
 		print(row)
-		
 
+	print("")
+	print(graph.greedy_coloring())
+		
 main()
